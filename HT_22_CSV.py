a44a2be1394eb404954bebaa37c2accedc3725b6
Andrii Tarasenko <333antar333@gmail.com>
2023-07-24 12:42:33 +0200

    fixed HT 22

--- HT_22_CSV.py
+++ HT_22_CSV.py
@@ -11,25 +11,18 @@ def create_phone_number(value: list) -> list:
     if random_numb < 75:
         value.append(random.choice(operators) + str(random.randint(0, 9999999)).zfill(7))
     else:
-        value
+        value.append("")
     return value
 
 
 with open("workers.json") as file:
     dict_workers = json.load(file)
 
-# в Windows нельзя добавлять латиницу и кирилицу в один файл поэтому я заменил 'ID' на 'ИН'
-# так же я заменил newline=None на newline="" и delimiter="," на delimiter=";"
-# а по хорошему надо Windows заменить на Ubuntu
-name_of_fields = ["ИН", "Имя", "Возраст", "Телефон"]
+# Я работаю на Windiws, по этому я заменил newline=None на newline="" и delimiter="," на delimiter=";"
+name_of_fields = ["ID", "Имя", "Возраст", "Телефон"]
 dict_workers = {key: create_phone_number(value) for (key, value) in dict_workers.items()}
 
-with open("workers_csv.csv", "w", newline="") as file:
+with open("workers_csv.csv", "w", newline="", encoding="utf-8-sig") as file:
     f_csv = csv.writer(file, delimiter=";")
     f_csv.writerow(name_of_fields)
-    for key, value in dict_workers.items():
-        item = [key, *value]
-        f_csv.writerow(item)
-    # в интернете поискал как это можно сделать оптимизированей и красивее и вот результат:
-    # f_csv.writerows([key, *value] for key, value in dict_workers.items())
-    # в свой код вставлять не буду, так как это не моё решение, но на будущее запомню.
+    f_csv.writerows([key, *value] for key, value in dict_workers.items())
