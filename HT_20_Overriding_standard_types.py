b7362e05221c2ec3b36550048bc00d7c6f2b3bef
Andrii Tarasenko <333antar333@gmail.com>
2023-07-28 09:01:00 +0200

    Add HT 20, fixed 19

---
+++ HT_20_Overriding_standard_types.py
@@ -0,0 +1,32 @@
+class String(str):
+
+    def __add__(self, other):
+        if str(self).isdigit() and str(other).isdigit():
+            return int(str(self) + str(other))
+        return str(self) + str(other)
+
+    def __sub__(self, other):
+        if str(self).count(str(other)):
+            self, other = str(self), str(other)
+            return self[:self.find(other)] + self[self.find(other) + len(other):]
+        else:
+            return self
+
+
+print(String('New') + String(890))
+print(String(1234) + 5678, end=" ")
+print(" -->", type(String(1234) + 5678))
+print(String('New') + 'castle')
+print(String('New') + 77)
+print(String('New') + True)
+print(String('New') + ['s', ' ', 23])
+print(String('New') + None)
+print("-" * 50)
+
+print(String('New bala7nce') - 7)
+print(String('New balance') - 'bal')
+print(String('New balance') - 'Bal')
+print(String('pineapple apple pine') - 'apple')
+print(String('New balance') - 'apple')
+print(String('NoneType') - None)
+print(String(55678345672) - 7)
